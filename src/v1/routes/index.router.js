277312971f4d1fbb0e719134adaa53454a2b29de
Đinh Công Thái <welterial@gmail.com>
2025-12-03 23:01:13 +0700

    FEAT: add api user

--- src/v1/routes/index.router.js
+++ src/v1/routes/index.router.js
@@ -1,11 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/checkstatus', (req, res, next) => {
+router.get("/checkstatus", (req, res, next) => {
     res.status(200).json({
-        status: 'success',
-        message: 'api ok'
-    })
-})
+        status: "success",
+        message: "api ok",
+    });
+});
+
+router.get("/api/users", (req, res, next) => {
+    res.status(200).json({
+        status: "sucess api",
+        message: "api ok",
+        metadata: [
+            {
+                name: "anonystick",
+                age: 40,
+            },
+            {
+                name: "Ronaldo",
+                age: 39,
+            },
+            {
+                name: "Messi",
+                age: 37,
+            },
+        ],
+    });
+});
 
 module.exports = router;
